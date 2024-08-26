e2820c1705e4c466d03f76520472c160e52bcdc8
clearwater-agency <developer@clearwateragency.com.au>
2024-08-26 18:23:33 +1000

    Formatting Changes

--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -1,11 +1,11 @@
-export default function Log({turns}) {
-    return (
-      <ol id="log">
-        {turns.map((turn, index) => (
-          <li key={index}>
-            {turn.player} selected: {turn.square[0]}, {turn.square[1]}
-          </li>
-        ))}
-      </ol>
-    );
-  }
+export default function Log({ turns }) {
+  return (
+    <ol id="log">
+      {turns.map((turn, index) => (
+        <li key={index}>
+          {turn.player} selected: {turn.square[0]}, {turn.square[1]}
+        </li>
+      ))}
+    </ol>
+  );
+}
